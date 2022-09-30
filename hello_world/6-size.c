be2f0844970123877ea1d0f39b39cf7d79dd17f3
rayaneRX <elkerayane@gmail.com>
2022-09-30 12:04:54 -0700

    gghd

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program that prints the size of various types
+ * Return: 0
+ */
+int main(void)
+{
+  char charType;
+  int intType;
+  long int longIntType;
+  long long int longLongIntType;
+  float floatType;
+
+  printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+  printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+  printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+  printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
+  printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
+  return (0);
+}
