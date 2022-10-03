b861677d28755b333268e44d647d3f84c4db8984
rayaneRX <elkerayane@gmail.com>
2022-10-03 08:08:26 -0700

    ccvc

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Program that prints the alphabet in lower & uppercase.
+* Return: 0
+*/
+int main(void)
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+putchar (x);
+for (x = 'A'; x <= 'Z'; x++)
+putchar (x);
+putchar ('\n');
+return (0);
+}
