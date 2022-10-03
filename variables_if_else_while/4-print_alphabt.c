784239998682a530b1f17fdd5a64c61e2b2d7279
rayaneRX <elkerayane@gmail.com>
2022-10-03 08:36:29 -0700

    ssds

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Program that prints the alphabet in lowercase,
+* except e and q.
+* Return: 0
+*/
+int main(void)
+
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+if (bo <= 'z' && bo != 'q' && bo != 'e')
+{
+putchar(bo);
+bo++;
+}
+else
+{
+bo++;
+}
+}
+putchar('\n');
+return (0);
+}
