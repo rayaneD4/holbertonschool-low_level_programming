c62416e6a8ad449f2038ab37a8e7c24d88b73d64
rayaneRX <elkerayane@gmail.com>
2022-10-03 02:57:03 -0700

    vvb

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description:program will assign a random number to the variable n.
+ * Return: 0
+ */
+int main(void)
+{
+  int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+primtf ("%d is positive\n,n);
+{
+else if (n == 0)
+{ 
+printf ("%d is zero\n,n);
+{
+ else (n < 0)
+{
+printf ( "%d is negative\n,n);
+{
+return (0);
+}
