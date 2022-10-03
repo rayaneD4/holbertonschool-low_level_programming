1b7ae8fb07817b9b0f35a6a9a34dbdc8c68fcc0a
rayaneRX <elkerayane@gmail.com>
2022-10-03 03:41:54 -0700

    ccv

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,15 @@
-include < stdlib.h >
+#include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
- * main - Entry point
- * Description:program will assign a random number to the variable n.
- * Return: 0
- */
+* main-Entry point
+*
+* Return:Always 0 (Success)
+*/
 int main(void)
 {
 int n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
@@ -19,7 +21,6 @@ else if (n == 0)
 printf("%d is zero\n", n);
 }
 else
-(n < 0)
 {
 printf("%d is negative\n", n);
 }
