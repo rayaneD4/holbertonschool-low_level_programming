7195d77bec35471a587b84770bdd5eb692a6c762
rayaneRX <elkerayane@gmail.com>
2022-10-03 05:58:18 -0700

    ccvc

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program that prints the last digit of the number stored
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+int dermer_chiffre_de_n ;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+demer_chiffre_de_n= m%10
+if (n > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n);
+}
+else if (n == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n);
+}
+else
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+}
+return (0);
+}
