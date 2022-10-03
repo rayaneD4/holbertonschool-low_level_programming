77ab77e5407f31364d07788e2e300d77a4f62488
rayaneRX <elkerayane@gmail.com>
2022-10-03 06:42:58 -0700

    ccvc

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,11 +13,11 @@ int boo;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 boo = n % 10;
-if (n > 5)
+if (boo > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, boo);
 }
-else if (n == 0)
+else if (boo == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, boo);
 }
