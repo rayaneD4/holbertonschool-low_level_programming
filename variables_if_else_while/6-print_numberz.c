49b82183f40ab5e7a36712775d08e1831182f749
rayaneRX <elkerayane@gmail.com>
2022-10-03 08:59:18 -0700

    ccvc

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program
+ * Return: 0
+ */
+int main(void)
+
+{
+int no = 0;
+while (no < 10)
+{
+putchar('0' + no);
+no++;
+}
+putchar('\n');
+return (0);
+}
