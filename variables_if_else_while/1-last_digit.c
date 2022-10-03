39fe5198080876bf04ce3ea45153206698c166ec
rayaneRX <elkerayane@gmail.com>
2022-10-03 06:23:49 -0700

    vv

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,21 +9,22 @@
 int main(void)
 {
 int n;
-int dermer_chiffre_de_n ;
+int dermer_chiffre_de_n;
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-demer_chiffre_de_n= m%10
+demer_chiffre_de_n = n % 10
 if (n > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n);
+printf("Last digit of %d is %d and is greater than 5\n", n,demer_chiffre_de_n);
 }
 else if (n == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n);
+printf("Last digit of %d is %d and is 0\n", n,demer_chiffre_de_n);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n,demer_chiffre_de_n);
 }
 return (0);
 }
