d58049c2729f152c2854588d04d5238e778a3f8e
rayaneRX <elkerayane@gmail.com>
2022-10-03 09:36:25 -0700

    ccxc

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program that prints the alphabet
+ * Return: 0
+ */
+int main(void)
+{
+  char nu = 0;
+  char cod = 'a';
+while (nu < 10)
+{
+putchar('0' + nu);
+nu++;
+}
+while (cod <= 'f')
+{
+putchar(cod);
+cod++;
+}
+putchar('\n');
+return (0);
+}
