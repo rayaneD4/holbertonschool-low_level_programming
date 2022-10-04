09f3bf51fb802cb6c61d83ffabc80905b4b05303
rayaneRX <elkerayane@gmail.com>
2022-10-04 03:23:53 -0700

    ccscsc

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_alphabet 
- *
- * Return: 0
- */
+* print_alphabet
+*
+* Return: 0
+*/
 void print_alphabet(void)
 {
 char boo = 'a';
- 
+
 while (boo <= 'z')
 {
 putchar(boo);
