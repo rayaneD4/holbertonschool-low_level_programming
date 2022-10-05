fb47660e9e0b07bf29e83452cb8e6a6fb9da91c0
rayaneRX <elkerayane@gmail.com>
2022-10-05 02:29:57 -0700

    zze

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - every minute of the day
+ *
+ */
+void jack_bauer(void)
+{
+int h;
+int m;
+for (h = 0; h < 24; h++)
+{
+for (m = 0; m < 60; m++)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+_putchar(':');
+_putchar(m / 10) + '0');
+_putchar(m % 10) + '0');
