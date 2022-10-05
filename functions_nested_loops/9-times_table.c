3aac35ec22c242540df3481bdbab8425991eef66
rayaneRX <elkerayane@gmail.com>
2022-10-05 06:15:23 -0700

    aaz

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -6,32 +6,37 @@
  */
 void times_table(void)
 {
-int na, nb;
+int na, nb, r;
 for (na = 0; na <= 9; na++)
 {
-for (nb = 0; nb <= 9; nb++)
-{
-int r = na * nb;
-if (nb > 0 && r < 10)
-_putchar(' ');
-_putchar(' ');
-}
-if (r <= 9)
-{
-_putchar('0' + r);
-}
-else if (r > 9)
-{
-int n1 = r / 10;
-int n2 = r % 10;
-_putchar('0' + n1);
-_putchar('0' + n2);
-}
-if (nb < 9)
-{
-_putchar(',');
-}
-}
+	for (nb = 0; nb <= 9; nb++)
+	{
+		r = na * nb;
+		if (nb > 0 && r < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		else if (r > 9)
+		{
+		_putchar(' ');
+		}
+	if (r <= 9)
+	{
+		_putchar('0' + r);
+	}
+	else if (r > 9)
+	{
+		int n1 = r / 10;
+		int n2 = r % 10;
+		_putchar('0' + n1);
+		_putchar('0' + n2);
+	}
+	if (nb < 9)
+	{
+	_putchar(',');
+	}
+	}
 _putchar('\n');
 }
 }
