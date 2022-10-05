317be916d59ec0e281a6014a4372164505adfd40
rayaneRX <elkerayane@gmail.com>
2022-10-05 01:24:24 -0700

    zza

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * digit - computes the absolute value of an integer.
+ * @w: integer
+ * Return: 0
+ */
+int print_last_digit(int w);
+{
+int digit = w % 10;
+if (w < 0)
+digit = digit * (-1);
+return (digit);
+}
