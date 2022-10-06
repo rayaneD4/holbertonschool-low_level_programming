b865026c77b0d7783e0acfb950b025b1e24e3270
rayaneRX <elkerayane@gmail.com>
2022-10-06 07:57:00 -0700

    zz

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: Number
+ * @b: Number
+ *
+ * Return: res (Success)
+ */
+int mul(int a, int b)
+{
+int r = a * b;
+return (r);
+}
