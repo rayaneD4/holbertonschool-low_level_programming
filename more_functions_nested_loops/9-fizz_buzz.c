c232eaaf8976fd60a17655e2bd116d11dc964d67
rayaneRX <elkerayane@gmail.com>
2022-10-07 02:02:29 -0700

    mmal

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Fizz-Buzz test
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+int s;
+for (s = 1; s <= 100; s++)
+{
+if (s % 3 == 0 && s % 5 == 0)
+printf("FizzBuzz ");
+else if (s % 3 == 0)
+printf("Fizz ");
+else if (s % 5 == 0)
+printf("Buzz ");
+else
+printf("%d ", s);
+}
+printf("\n");
+return (0);
+}
