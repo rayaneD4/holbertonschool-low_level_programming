fab5a99d1be86123363098284e42197694f97b2d
rayaneRX <elkerayane@gmail.com>
2022-10-10 03:53:57 -0700

    mmlmz

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * strlen - returns the length of a string
+ * @s: string
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+int n = 0;
+
+while (*s != '\0')
+{
+n++;
+s++;
+
+}
+return (n);
+}
