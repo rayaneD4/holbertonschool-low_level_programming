05e1e395047e19ee3ead7a1e052e70bb20d5f08e
rayaneRX <elkerayane@gmail.com>
2022-10-10 04:07:05 -0700

    mmlmlmlm

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string
+ *
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
