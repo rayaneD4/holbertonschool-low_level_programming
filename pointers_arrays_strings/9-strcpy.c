f981d3dac9d40d0221ec0ecfe29a6e4fc058074f
rayaneRX <elkerayane@gmail.com>
2022-10-11 06:54:29 -0700

    amamam

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - function that copy the string
+ * @dest: string🎶
+ * @src: string
+ * Return: 0
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+int n = strlen(src)
+for (i = 0; i <= n; i++)
+{
+dest[i] = src[i];
+}
+}
