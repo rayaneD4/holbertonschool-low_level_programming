cfe76d0844f57c9f75d53d933f4fd763f73bf1cf
rayaneRX <elkerayane@gmail.com>
2022-10-11 04:17:43 -0700

    mamam

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -7,16 +7,16 @@
  */
 void puts2(char *str)
 {
-int i;
+int i, h;
 int n = strlen(str);
 
 if (n % 2 == 0)
-int h = n / 2;
+h = n / 2;
 else
-int h = (n / 2) + 1;   
+h = (n / 2) + 1;   
 for (i = h; i < n; i++)
 {
 putchar(*(str + i));
 }
-putchar('\n')
+putchar('\n');
 }
