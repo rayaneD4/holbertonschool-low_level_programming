668e8b1368ab58f1e90c1268b33df090d11426d8
rayaneRX <elkerayane@gmail.com>
2022-10-12 06:58:43 -0700

    zz

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * cap_string - Function that capitalizes all words of a string
- * @s: variable 
+ * @s: variable
  * Return: 0
  */
 char *cap_string(char *s)
@@ -13,18 +13,16 @@ while (s[i] != '\0')
 {
 if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 {
+
+if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
 s[i] = s[i] - 32;
-i++;
-}
-if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'|| s[i] == ',' || s[i] == ';' || s[i] == '.'|| s[i] == '!' || s[i] == '?' || s[i] == '"'|| s[i] == '(' || s[i] == ')' || s[i] == '{'|| s[i] == '}')
-{
-i++;
-if (s[i] >= 'a' && s[i] <= 'z')
-{
+if (s[i] == ',' || s[i] == ';' || s[i] == '.')
+s[i] = s[i] - 32;
+if (s[i] == '!' || s[i] == '?' || s[i] == '"')
+s[i] = s[i] - 32;
+if (s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
 s[i] = s[i] - 32;
 }
-}
-else
 i++;
 }
 return (s);
