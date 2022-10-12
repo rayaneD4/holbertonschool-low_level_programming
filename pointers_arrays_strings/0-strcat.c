5fc20b17a8c08f839ad18819a2f9eb77641d8862
rayaneRX <elkerayane@gmail.com>
2022-10-12 02:44:52 -0700

    amam

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,37 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _strcat - concatenates two strings
- * @src: chaine premier
- * @dest: chaine deuxieme
- *
+ * _strlen - Entry point
+ * @dest: pointer string
  * Return: 0
  */
 
-int _strlen(char *s)
+int _strlen(char *dest)
 {
 int n = 0;
 
-while (s[n] != '\0')
+while (dest[n] != '\0')
 {
 n++;
 }
 return (n);
 }
-
+/**
+ * _strcat - Entry point
+ * @dest: pointer string
+ * @src: pointer string
+ *
+ * Return:0
+ */
 char *_strcat(char *dest, char *src)
 {
 int len = _strlen(dest);
 int i;
 
 for (i = 0; src[i] != '\0'; i++)
-{
 dest[len + i] = src[i];
 dest[len + i] = '\0';
+
+return (dest);
 }
-return(dest);
-}
+
