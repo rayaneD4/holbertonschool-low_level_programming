23061e01f5246c3367d602b92c1640f915c963ec
rayaneRX <elkerayane@gmail.com>
2022-10-12 05:05:37 -0700

    aamlml

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncpy - Entry point
+ * @dest: string
+ * @src: pointer 
+ * @n: integer
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
+}
