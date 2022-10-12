f7e162cbaccc7fb4f74f00369b1b0e5de1360a6f
rayaneRX <elkerayane@gmail.com>
2022-10-12 02:27:41 -0700

    ammama

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,11 +12,9 @@ int _strlen(char *s)
 {
 int n = 0;
 
-while (*s != '\0')
+while (s[n] != '\0')
 {
 n++;
-s++;
-
 }
 return (n);
 }
@@ -25,10 +23,11 @@ char *_strcat(char *dest, char *src)
 {
 int len = _strlen(dest);
 int i;
-for (i = 0; src[i] != '\0';i++)
+
+for (i = 0; src[i] != '\0'; i++)
 {
-dest[len+1] = src[i];
-dest[len+1] = '\0';
+dest[len + i] = src[i];
+dest[len + i] = '\0';
 }
 return(dest);
 }
