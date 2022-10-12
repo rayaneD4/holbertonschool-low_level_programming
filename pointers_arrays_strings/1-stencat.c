1e105706b8d66d6ed0025fe6ec4571dd769d1260
rayaneRX <elkerayane@gmail.com>
2022-10-12 03:38:57 -0700

    amamam

---
+++ pointers_arrays_strings/1-stencat.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: pointer string
+ *
+ * Return: i
+ */
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*s != '\0')
+{
+i++;
+s++;
+
+}
+return (i);
+}
+
+/**
+ * _strncat - Entry point
+ * @dest: pointer string
+ * @src: pointer string
+ * @n: integer
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i;
+int len = _strlen(dest);
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[len + i] = src[i];
+}
+dest[len + i] = '\0';
+return (dest);
+}
