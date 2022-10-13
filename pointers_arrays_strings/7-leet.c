5b2e2fd8680337af278dc096d995fab646c39e11
rayaneRX <elkerayane@gmail.com>
2022-10-13 03:12:07 -0700

    aal

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - function that prints half of the string
+ * @s: variable with the string
+ * Return: 0
+ */
+char *leet(char *s)
+{
+int i;
+char *s = l
+char a[] = {'A', 'E', 'O', 'T', 'L'};
+char n[] = {'4', '3', '0', '7', '1'};
+while (*s != '\0')
+{
+for (i = 0; i < 5; i++)
+{
+if (*s == a[i] || *s == a[i] + 32)
+*s = n[i];
+}
+}
+return (s);
+}
