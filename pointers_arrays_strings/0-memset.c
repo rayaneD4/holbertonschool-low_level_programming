cd53760249e92122c597870c186f192c7ce40107
rayaneRX <elkerayane@gmail.com>
2022-10-13 06:43:25 -0700

    aamlml

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - Entry point
+ * @s: pointer
+ * @b: character
+ * @n: integer
+ *
+ * Return: Always s (Success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+char *m;
+size_t i;
+
+c = m;
+i = 0;
+while (i < n)
+{
+m[i] = (char)c
+i++;
+}
+return (s);
+}
