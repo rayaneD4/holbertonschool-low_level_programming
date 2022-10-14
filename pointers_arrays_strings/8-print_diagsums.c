3a10c089884c84c888310f280386234d63d05887
rayaneRX <elkerayane@gmail.com>
2022-10-14 03:37:07 -0700

    aaammlm

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,7 +9,7 @@
  */
 void print_diagsums(int *a, int size)
 {
-int i, sum1, sum2;
+int i, j, sum1, sum2;
 
 sum1 = 0;
 sum2 = 0;
@@ -17,7 +17,7 @@ for (i = 0; i < size * size ; i = i + size + 1)
 {
 sum1 = sum1 + a[i];
 }
-for (i = size - 1; i < (size * size) - 1;)
+for (j = size - 1; j < (size * size) - 1;)
 {
 sum2 = sum2 + a[i];
 }
