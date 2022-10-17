6dc10c9432666f5f13f8339d4f1d3ddd36a85317
rayaneRX <elkerayane@gmail.com>
2022-10-17 06:11:31 -0700

    ddffd

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -8,11 +8,11 @@
 
 int _strlen_recursion(char *s)
 {
-  if (*s != '\0')
-    {
-      s++;
-      return (_strlen_recursion(s) + 1);
-    }
-  else
+if (*s != '\0')
+{
+s++;
+return (_strlen_recursion(s) + 1);
+}
+else
 return (0);
 }
