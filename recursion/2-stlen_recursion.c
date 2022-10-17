576fecea6af7272dd09624d85c7a1580b6eae603
rayaneRX <elkerayane@gmail.com>
2022-10-17 06:05:24 -0700

    fff

---
+++ recursion/2-stlen_recursion.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * _strlen_recursion - prints the length of a string
+ * @s: random value
+ * Return: 0 otherwise 1
+ */
+
+int _strlen_recursion(char *s)
+{
+  if (*s != '\0')
+    {
+      s++;
+      return (_strlen_recursion(s) + 1);
+    }
+  else
+return (0);
+}
