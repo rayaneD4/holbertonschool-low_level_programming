f3187b748c19554fe378f634ef17d5f08dc18588
rayaneRX <elkerayane@gmail.com>
2022-10-18 03:43:43 -0700

    nfnfnnf

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - Entry point
+ * @x: Integer
+ * @y: Integer
+ *
+ * Return: integer (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+if (y > 0)
+return (x * _pow_recursion(x, y - 1));
+else if (y < 0)
+return (-1);
+else (y == 1)
+return (1);
+}
