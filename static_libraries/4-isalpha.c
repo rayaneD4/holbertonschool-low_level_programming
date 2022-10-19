16bb5907ac68b855de9dbf8068b61a6114f743f2
rayaneRX <elkerayane@gmail.com>
2022-10-19 07:01:33 -0700

    dffee

--- static_libraries/4-isalpha.c
+++ static_libraries/4-isalpha.c
@@ -8,9 +8,9 @@
 int _islower(int c)
 {
   if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-				      {
-					return (1);
-				      }
+{
+  return (1);
+}
   else
     {
       return (0);
