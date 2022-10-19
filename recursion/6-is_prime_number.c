5b941f1607f569b53819c2f7854fdc2ab28fa59a
rayaneRX <elkerayane@gmail.com>
2022-10-19 02:41:36 -0700

    fefrfr

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * prime - check prime number
+ * @b: Integer
+ * @i: Integer
+ *
+ * Return: 0
+ */
+int prime(int b, int i)
+{
+if (b == i)
+return (1);
+if (!(b % i))
+return (0);
+else
+return (prime(b, i + 1));
+}
+
+/**
+ * is_prime_number - give the square
+ * @n: Integer
+ *
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+return (prime(n, 2));
+}
