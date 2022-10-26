c6e8d80e2afb39fadad12517a42b82127d998430
rayaneRX <elkerayane@gmail.com>
2022-10-26 05:53:56 -0700

    ededde

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -10,11 +10,12 @@
 int *array_range(int min, int max)
 {
 int *ar;
-int i = 0;
-int sum = max - (min + 1);
+int i;
+int sum = max - min + 1;
+
 if (min > max)
 return (NULL);
-ar = malloc(sizeof(int) * (sum));
+ar = malloc(sizeof(int) * sum);
 if (ar == NULL)
 return (NULL);
 for (i = 0; i < sum; i++)
