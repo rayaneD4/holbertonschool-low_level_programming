0ac651040f9f2b04fb6a39cdfe4f684dace4a5eb
rayaneRX <elkerayane@gmail.com>
2022-10-26 04:37:05 -0700

    eeded

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,16 +14,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 char *ptr;
 unsigned int i;
 
-		if (nmemb == 0 || size == 0)
-			return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
 
-			ptr = malloc(size * nmemb);
-		if (ptr == NULL)
-	return (NULL);
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+return (NULL);
 
-	for (i = 0; i < (size * nmemb); i++)
-		{
-			ptr[i] = '\0';
-		}
-			return (ptr);
+for (i = 0; i < (size * nmemb); i++)
+{
+ptr[i] = '\0';
+}
+return (ptr);
 }
