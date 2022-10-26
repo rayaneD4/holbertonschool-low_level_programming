8d0f4a2f4d6983651d32adbf8190510c64e47c90
rayaneRX <elkerayane@gmail.com>
2022-10-26 04:04:47 -0700

    concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,38 +15,35 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *c;
 unsigned int g1, g2;
 
- if (s1 == NULL)
-   s1 = "";
+if (s1 == NULL)
+s1 = "";
+for (g1 = 0; s1[g1] != '\0'; g1++)
+{
+;
+}
 
- for (g1 = 0; s1[g1] != '\0'; g1++)
-   {
-     ;
-   }
+if (s2 == NULL)
+s2 = "";
 
- if (s2 == NULL)
-   s2 = "";
+for (g2 = 0; s2[g2] != '\0'; g2++)
+{
+;
+}
+if (n > g2)
+n = g2;
 
- for (g2 = 0; s2[g2] != '\0'; g2++)
-   {
-     ;
-   }
- if (n > g2)
-   n = g2;
+c = malloc(sizeof(char) * (n + g1 + 1));
 
- c = malloc(sizeof(char) * (n + g1 + 1));
+if (c == NULL)
+return (NULL);
+for (g1 = 0; s1[g1] != '\0'; g1++)
+c[g1] = s1[g1];
+for (g2 = 0; g2 < n; g2++)
+{
+c[g1] = s2[g2];
+g1++;
+}
+c[g1] = '\0';
 
- if (c == NULL)
-   return (NULL);
-
- for (g1 = 0; s1[g1] != '\0'; g1++)
-   c[g1] = s1[g1];
- for (g2 = 0; g2 < n; g2++)
-   {
-     c[g1] = s2[g2];
-     g1++;
-   }
-
- c[g1] = '\0';
-
- return (c);
+return (c);
 }
