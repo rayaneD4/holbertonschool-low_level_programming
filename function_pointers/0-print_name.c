0316002c92cd472b057282baf9c98e91ac874421
rayaneRX <elkerayane@gmail.com>
2022-11-01 07:17:05 -0700

    dedede

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name
+ * @f: pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+if (f != NULL)
+f(name);
+}
