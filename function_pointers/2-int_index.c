a53150b2a75805cbd9e23e64796130b2903af167
rayaneRX <elkerayane@gmail.com>
2022-11-02 04:23:12 -0700

    tohell

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - searches for an integer
+ * @array: array
+ * @size: size
+ * @cmp: pointer
+ *
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, l = size;
+	
+	if (size <= 0)
+		return (-1);
+	if (array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0, i < l; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (0);
+}
+
+
