7ccc0ad7297184430a9ee9c83a94bbfa36aa6c60
rayaneRX <elkerayane@gmail.com>
2022-11-03 03:39:58 -0700

    va_list

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,18 +11,18 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list pr;
-unsigned int sum = 0, i;
+	unsigned int sum = 0, i;
 
 	if (n == 0)
-		
+	{
 		return (0);
-	
-	va_start(pr, n);
-	
+	}
+	va_start(pt, n);
+
 	for (i = 0; i < n; i++)
-
-			sum += va_arg(pr, int);
-
+	{
+		sum += va_arg(pr, int);
+	}
 	va_end(pr);
 		return (sum);
 }
