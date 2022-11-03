c4fab53f14ccdc7e89a72627787bb370c853c951
rayaneRX <elkerayane@gmail.com>
2022-11-03 03:10:47 -0700

    va_list

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - the sum of all its parameters
+ * @n: integer
+ *
+ * Return: 0
+ **/
+int sum_them_all(const unsigned int n, ...)
+{
+va_list pr;
+	int sum = 0, i;
+
+	if (n == 0)
+		
+		return (0);
+	
+	va_start(pr, n);
+	
+	for (i = 0; i < n; i++)
+
+			sum += va_arg(pr, int);
+
+	va_end(pr);
+		return (sum);
+}
