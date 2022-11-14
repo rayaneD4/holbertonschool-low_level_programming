38206ad1f2131979c8537d1dcc9a3955481877cc
rayaneRX <elkerayane@gmail.com>
2022-11-14 07:11:04 -0800

    add 1-liste

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * list_len -  function that returns the number of elements
+ * @h: number
+ *
+ * Return: 0
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+int i = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h == NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
+
+
