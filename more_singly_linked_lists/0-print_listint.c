4d90d11f96d9f454b1045bbbbde24c5b27fd47f4
rayaneRX <elkerayane@gmail.com>
2022-11-15 06:45:36 -0800

    prints all the elements with printf

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -7,10 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-int i = 0;
-	for (i = 0; h != NULL; i++)
+	int i = 0;
+
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
-		
-	h = h->next;
+		i++;
+		h = h->next;
+	}
 	return (i);
 }
