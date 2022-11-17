6bf2bbfa40541116adcf2302a093295f6b6ab796
rayaneRX <elkerayane@gmail.com>
2022-11-17 04:06:43 -0800

    add the lists.h

--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -1,9 +1,8 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef lists_h
+#define lists_h
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,12 +13,12 @@
  */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
-size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 void free_listint2(listint_t **head);
