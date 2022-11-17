cc3f3f0754d33e3ca99c20a53c9339b18160cdc1
rayaneRX <elkerayane@gmail.com>
2022-11-17 06:56:38 -0800

    free a node

---
+++ more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint2 -  frees a listint_t list
+ * @head: pointer
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+
+	while (tmp != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
