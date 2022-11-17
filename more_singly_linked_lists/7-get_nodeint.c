cc523b5a64cbf4bfa5c0314525a0f4a1f1c3eea3
rayaneRX <elkerayane@gmail.com>
2022-11-17 08:36:31 -0800

    returns the nth node

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+# include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node
+ * @head: poitor
+ *
+ * Return: 0
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	 
+	if (head == NULL)
+	return (0);
+
+	for(i = 0; i < index && head != NULL; i++)
+    {
+        head = head->next;
+    }
+return (head);
+}
