70a91cfe97e4658c82ce26feb994e8cc8fdf5674
rayaneRX <elkerayane@gmail.com>
2022-11-17 08:59:48 -0800

    eturns the sum of all the data (n)

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+# include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data (n)
+ * @head: head node
+ *
+ * Return: 0
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0, tmp;
+	
+	if (head == NULL)
+	return (0);
+
+	while (head != NULL)
+	{
+		tmp = head->n;
+		sum += tmp;
+		head = head->next;
+	}
+	return (sum);
+}
