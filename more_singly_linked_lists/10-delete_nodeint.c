28fc673378e9ba4e7bcbe448f7d4c635a6afa44e
rayaneRX <elkerayane@gmail.com>
2022-11-18 08:45:11 -0800

    unction that deletes the node at index

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * @head: node head
+ * @index: index
+ *
+ * Return: 0
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+	listint_t *tmp = *head, *len;
+	unsigned int i;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = tmp->next;
+		free(tmp);
+		return (1);
+	}
+
+	for (i = 0; tmp != NULL && i < index; i++)
+	{
+		tmp = tmp->next;
+	}
+	if (tmp == NULL)
+		return (-1);
+
+	len = tmp->next->next;
+	free(tmp->next);
+	tmp->next = len;
+
+	return (1);
+}
