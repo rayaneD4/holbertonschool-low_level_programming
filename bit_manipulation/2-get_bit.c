6bfd3e6d76f190bdce13d7c134b7ed75843eec9f
rayaneRX <elkerayane@gmail.com>
2022-11-20 05:28:36 -0800

    add all

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,7 +11,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int res = (n >> index) & 1;
 
-	if (index == NULL)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 	
 	return (res);
