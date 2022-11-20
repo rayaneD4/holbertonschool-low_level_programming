3d23ef66c0a4516b59e15b25916a88d7a533590a
rayaneRX <elkerayane@gmail.com>
2022-11-20 07:30:43 -0800

    function that prints the binary representation of a number.

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_binary - call point
+ * @n: decimal number to convert
+ * Return: 0 
+ */
+void print_binary(unsigned long int n)
+{
+	long int i = 0, size;
+	unsigned long int y, k;
+	
+	if (n == 0)
+		printf("0");
+	
+	y = n;
+	
+	while (y != '\0')
+	{
+		i++;
+		y = y >> 1;
+	}
+	
+	for (size = i - 1; size >= 0; size--)
+	{
+		k = n >> size;
+		if (k & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+}
