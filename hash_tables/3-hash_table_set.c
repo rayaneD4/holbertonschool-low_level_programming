fa1c1563aca1079cc607b05f79b8dc14d036a903
rayaneRX <elkerayane@gmail.com>
2022-11-26 14:37:20 -0800

    add 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,2 +1,55 @@
+#include "hash_tables.h"
 
-ibo
+/**
+ * hash_table_set - adds an element to the hash table
+ *
+ * @ht: hash table to add or update the key/value to
+ * @key: the key who is not an empty string
+ * @value: the value associated with the key.
+ *
+ * Return: 1 if success or 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_dup;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	value_dup = strdup(value);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_dup;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_dup);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_dup;
+	if (value_dup == NULL)
+		return (0);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
+
