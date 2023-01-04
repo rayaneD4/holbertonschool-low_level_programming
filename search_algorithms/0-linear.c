84d88555de93f47615e765070889101cab26567a
rayaneRX <elkerayane@gmail.com>
2023-01-04 12:28:32 +0100

    add the linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%ld] = [%d]\n", index, array[index]);
 		if (array[index] == value)
-			return (index);
+			return (index);mll
 	}
 	return (-1);
 }
