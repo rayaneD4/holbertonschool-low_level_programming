f3249b0dd1b530837db7a7274d54bbfde48ff61d
rayaneRX <elkerayane@gmail.com>
2023-01-04 02:32:41 +0100

    add the linear

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  * @array: is a pointer to the first element of
  * the array to search in
  * @size: is the number of elements in array
- * @value: is the value to search for
+ * @value: is the value to search for 
 */
 
 
