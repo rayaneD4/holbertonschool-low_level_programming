659f12c504882159991983b488a318084ce885df
rayaneRX <elkerayane@gmail.com>
2023-01-05 14:39:26 +0100

    add the 1

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -12,33 +12,32 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    int low = 0;
-    int high = size - 1;
-    int mid = 0;
-    int i;
+	int low = 0;
+	int high = size - 1;
+	int mid = 0;
+	int i;
 
-    while (low <= high)
-    {
-        mid = (low + high) / 2;
-        printf("Searching in array:");
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array:");
 
-        for (i = low; i <= high; i++)
-        {
-            printf(" %d", array[i]);
-            if (i < high)
-            printf(",");
-        }
-       
-        
-        printf("\n");
-        if (array[mid] == value) 
-            return (mid);
+		for (i = low; i <= high; i++)
+		{
+			printf(" %d", array[i]);
+			if (i < high)
+			printf(",");
+		}
 
-        else if (array[mid] < value)
-            low = mid + 1;
-        
-        else
-            high = mid - 1;
-    }
-    return (-1);
+		printf("\n");
+		if (array[mid] == value) 
+			return (mid);
+
+		else if (array[mid] < value)
+			low = mid + 1;
+
+		else
+			high = mid - 1;
+	}
+	return (-1);
 }
